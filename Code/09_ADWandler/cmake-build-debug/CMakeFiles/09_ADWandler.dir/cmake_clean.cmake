8f12bde2cc97a28d8288c16635c846946f8084e2
ReneSteinmaurer <55176260+ReneSteinmaurer@users.noreply.github.com>
2021-04-22 12:12:48 +0200

    Protokoll hinzugefügt

---
+++ Code/09_ADWandler/cmake-build-debug/CMakeFiles/09_ADWandler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/09_ADWandler.dir/main.c.o"
+  "09_ADWandler.pdb"
+  "09_ADWandler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/09_ADWandler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
